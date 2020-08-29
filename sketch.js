2f57878b5b303920fd2024a84bf1eab024d4008b
Anamika93-patel <66108521+Anamika93-patel@users.noreply.github.com>
2020-08-30 00:22:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,12 +8,13 @@ function setup() {
   weight=random(400,1500)
   car=createSprite(50,200,50,50)
   wall=createSprite(1500,200,60,height/2)
+  car.velocityX = speed;
 }
 
 function draw() {
   background(0);
 
-  car.velocityX = speed;
+  
 
   if(wall.x-car.x < (car.width + wall.width/2)){
      car.velocityX=0;
